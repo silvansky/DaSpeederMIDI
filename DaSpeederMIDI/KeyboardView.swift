14a64ee0ef24736fb2fbc0628b72e6c73e40687a
Valentine Silvansky <v.silvansky@gmail.com>
2026-01-27 16:50:28 +0300

    extend keyboard to C2-C7 for full 0.25x-4x range

--- DaSpeederMIDI/KeyboardView.swift
+++ DaSpeederMIDI/KeyboardView.swift
@@ -6,7 +6,7 @@ struct KeyboardView: View {
     var onNoteOn: ((UInt8) -> Void)?
 
     var body: some View {
-        Keyboard(layout: .piano(pitchRange: Pitch(48) ... Pitch(72)),
+        Keyboard(layout: .piano(pitchRange: Pitch(36) ... Pitch(84)),
                  noteOn: { pitch, _ in onNoteOn?(UInt8(pitch.intValue)) },
                  noteOff: { _ in })
             .frame(height: 120)
